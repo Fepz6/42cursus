0ba7862bdf18d06b6baf79ee1781d1412da4bec1
Felipe Jose Costa E Silva <fcosta-e@c1r1s7.42porto.com>
2023-11-22 21:15:36 +0000

    42

--- 42cursus/PIPEX/pipex.c
+++ 42cursus/PIPEX/pipex.c
@@ -10,47 +10,67 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#include "pipex.h" 
 
 int main(int argc, char **argv, char **envp)
 {
-    t_pipex *pipex;
+    t_pipex *pipex; /
     int i;
     int status;
     
+    // Verifica se o número de argumentos é correto
     if (argc != 5)
         incorrect_argument();
+    
     i = -1;
+    // Inicializa a estrutura t_pipex com variáveis relevantes
     pipex = init_pipex(envp);
+    
+    // Loop para criar dois processos filhos
     while (++i < 2)
     {
-        pipex -> pid[i] = fork();
-        if (pipex -> pid[i] < 0)
+        // Cria um processo filho
+        pipex->pid[i] = fork();
+        
+        // Sai em caso de falha na criação do processo filho
+        if (pipex->pid[i] < 0)
             free_exit(pipex, EXIT_FAILURE);
-        if (!pipex -> pid[i])
+        
+        // Se for um processo filho, executa a lógica do filho
+        if (!pipex->pid[i])
             child(pipex, argv, envp, i);
     }
+    
+    // Fecha e espera pelos processos filhos
     close_wait(pipex, &status);
+    
+    // Libera recursos e sai com status de saída dos processos, se estes terminaram normalmente
     if (WIFEXITED(status))
         free_exit(pipex, WEXITSTATUS(status));
+    
+    // Libera recursos e sai com status, caso contrário
     free_exit(pipex, status);
 }
 
+// Encontra o caminho (PATH) no ambiente ou usa um caminho padrão se não estiver definido
 char **find_path(char **envp)
 {
     int i;
-    char    **path;
-    char    *temp;
+    char **path;
+    char *temp;
     
     i = -1;
+    // Percorre o ambiente em busca do PATH
     while (envp && envp[++i])
     {
         if (ft_strncmp(envp[i], "PATH=", 5) == 0)
         {
+            // Separa o PATH em diferentes diretórios
             path = ft_split(envp[i] + 5, ':');
             i = -1;
             while (path[++i])
             {
+                // Adiciona uma barra no final de cada diretório
                 temp = path[i];
                 path[i] = ft_strjoin(path[i], "/");
                 free(temp);
@@ -58,26 +78,32 @@ char **find_path(char **envp)
             return (path);
         }
     }
+    // Se não encontrar o PATH, usa um padrão (/usr/bin/)
     path = ft_split("/usr/bin/", ' ');
     return (path);
 }
 
-char    *get_command(t_pipex *pipex, char **argument)
+// Verifica se um comando existe no caminho encontrado ou nos caminhos padrão, retorna o caminho completo para o comando
+char *get_command(t_pipex *pipex, char **argument)
 {
     int i;
-    char    *string;
+    char *string;
 
     i = 0;
+    // Verifica se o comando existe diretamente no diretório atual
     if (!access(argument[0], F_OK))
     {
+        // Verifica se o comando é executável
         if (!access(argument[0], X_OK))
             return (argument[0]);
         else
             exit(126);
     }
-    while (pipex -> path[i])
+    
+    // Verifica nos caminhos encontrados no PATH
+    while (pipex->path[i])
     {
-        string = ft_strjoin(pipex -> path[i], argument[0]);
+        string = ft_strjoin(pipex->path[i], argument[0]);
         if (!access(string, F_OK))
         {
             if (!access(string, X_OK))
@@ -87,50 +113,78 @@ char    *get_command(t_pipex *pipex, char **argument)
         }
         i++;
     }
+    
+    // Se não encontrar, define o erro como "No such file or directory"
     errno = ENOENT;
     return (NULL);
 }
 
-void    child(t_pipex *pipex, char **argv, char **envp, int i)
+// Lógica executada por um processo filho
+void child(t_pipex *pipex, char **argv, char **envp, int i)
 {
-    char    **command;
+    char **command;
     
+    // Configuração dos pipes para redirecionamento
     piping(pipex, argv, i);
+    
+    // Separa os argumentos do comando
     command = ft_split(argv[i + 2], ' ');
     if (get_command(pipex, command) == NULL)
     {
-        dup2(2,1);
-        //printf
+        // Se não encontrar o comando, redireciona o erro para o stderr e sai com um status específico
+        dup2(2, 1);
         exit(127);
     }
+    
+    // Executa o comando
     execve(get_command(pipex, command), command, envp);
+    
+    // Sai em caso de falha na execução do comando
     free_exit(pipex, EXIT_FAILURE);
 }
 
-void    piping(t_pipex *pipex, char **argv, int i)
+// Configura a lógica de redirecionamento de entrada e saída dos processos filhos usando pipes
+void piping(t_pipex *pipex, char **argv, int i)
 {
-    if (i ==0)
+    if (i == 0)
     {
-        pipex -> inputfd = open(argv[1], O_RDONLY);
-        if (pipex -> inputfd < 0)
+        // Se for o primeiro processo filho, configura a entrada
+        pipex->inputfd = open(argv[1], O_RDONLY);
+        if (pipex->inputfd < 0)
             perror("pipex: input");
-        if (dup2(pipex -> inputfd, STDIN_FILENO) < 0)
+        
+        // Redireciona a entrada para o arquivo ou pipe
+        if (dup2(pipex->inputfd, STDIN_FILENO) < 0)
             free_exit(pipex, EXIT_FAILURE);
-        close(pipex -> inputfd);
-        if (dup2(pipex -> pipefd[1], STDOUT_FILENO) < 0)
+        
+        // Fecha o descritor de arquivo da entrada
+        close(pipex->inputfd);
+        
+        // Redireciona a saída para o próximo processo
+        if (dup2(pipex->pipefd[1], STDOUT_FILENO) < 0)
             free_exit(pipex, EXIT_FAILURE);
     }
     else
     {
-        pipex -> outputfd = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
-        if (pipex -> outputfd < 0)
+        // Se for o segundo processo filho, configura a saída
+        pipex->outputfd = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+        if (pipex->outputfd < 0)
             perror("pipex: output");
-        if (dup2(pipex -> outputfd, STDOUT_FILENO) < 0)
+        
+        // Redireciona a saída para o arquivo de saída ou pipe
+        if (dup2(pipex->outputfd, STDOUT_FILENO) < 0)
             free_exit(pipex, EXIT_FAILURE);
-        close (pipex -> outputfd);
-        if (dup2(pipex -> pipefd[0], STDIN_FILENO) < 0)
-            free_exit(pipex, EXIT_FAILURE);  
+        
+        // Fecha o descritor de arquivo da saída
+        close(pipex->outputfd);
+        
+        // Redireciona a entrada para receber dados do processo anterior
+        if (dup2(pipex->pipefd[0], STDIN_FILENO) < 0)
+            free_exit(pipex, EXIT_FAILURE);
     }
-    close(pipex -> pipefd[0]);
-    close(pipex -> pipefd[1]);
+    
+    // Fecha os descritores de arquivo dos pipes
+    close(pipex->pipefd[0]);
+    close(pipex->pipefd[1]);
 }
+
--- 42cursus/PIPEX/pipex_utils.c
+++ 42cursus/PIPEX/pipex_utils.c
@@ -10,63 +10,75 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
-
-void    free_doubleptr(char **ptr)
+// Libera a memória alocada para um array de strings e para o próprio array
+void free_doubleptr(char **ptr)
 {
     int i;
 
     i = 0;
     while (ptr[i])
     {
-        free(ptr[i]);
+        free(ptr[i]); // Libera cada string dentro do array
         i++;
     }
-    free(ptr);
+    free(ptr); // Libera o array de strings
 }
 
-void    incorrect_argument(void)
+// Reporta um erro de argumento incorreto e termina o programa
+void incorrect_argument(void)
 {
-    errno = EINVAL;
-    perror(NULL);
-    exit(EINVAL);
+    errno = EINVAL; // Define o código de erro como "Invalid argument"
+    perror(NULL); // Exibe a mensagem de erro correspondente a EINVAL
+    exit(EINVAL); // Sai do programa com o código de erro EINVAL
 }
 
-void    close_wait(t_pipex *pipex, int *status)
+// Fecha os pipes e espera pelos processos filhos
+void close_wait(t_pipex *pipex, int *status)
 {
-    close(pipex -> pipefd[0]);
-    close(pipex -> pipefd[1]);
-    waitpid(pipex -> pid[0], status, 0);
-    waitpid(pipex -> pid[1], status, 0);
+    close(pipex->pipefd[0]); // Fecha o lado de leitura do pipe
+    close(pipex->pipefd[1]); // Fecha o lado de escrita do pipe
+    waitpid(pipex->pid[0], status, 0); // Espera pelo primeiro processo filho
+    waitpid(pipex->pid[1], status, 0); // Espera pelo segundo processo filho
 }
 
-void    free_exit(t_pipex *pipex, int exit_status)
+// Libera a memória alocada para a estrutura t_pipex e termina o programa com um status de saída
+void free_exit(t_pipex *pipex, int exit_status)
 {
-    free_doubleptr(pipex -> path);
-    free(pipex -> pid);
-    free(pipex);
-    exit (exit_status);
+    free_doubleptr(pipex->path); // Libera a memória alocada para o PATH
+    free(pipex->pid); // Libera a memória alocada para os PIDs dos processos filhos
+    free(pipex); // Libera a memória alocada para a estrutura t_pipex
+    exit(exit_status); // Sai do programa com o status de saída fornecido
 }
 
+// Inicializa a estrutura t_pipex alocando memória e configurando os pipes
 t_pipex *init_pipex(char **envp)
 {
     t_pipex *pipex;
     
+    // Aloca memória para a estrutura t_pipex
     pipex = malloc(sizeof(t_pipex));
     if (!pipex)
-        exit(EXIT_FAILURE);
-    pipex -> pid = malloc(2 * sizeof(pid_t));
-    if (!pipex -> pid)
+        exit(EXIT_FAILURE); // Sai se a alocação de memória falhar
+    
+    // Aloca memória para os PIDs dos processos filhos
+    pipex->pid = malloc(2 * sizeof(pid_t));
+    if (!pipex->pid)
     {
-        free(pipex);
-        exit(EXIT_FAILURE);
+        free(pipex); // Libera a memória alocada para t_pipex se a alocação falhar
+        exit(EXIT_FAILURE); // Sai se a alocação de memória falhar
     }
-    if (pipe(pipex -> pipefd) < 0)
+    
+    // Cria um pipe para comunicação entre os processos
+    if (pipe(pipex->pipefd) < 0)
     {
-        free(pipex -> pid);
-        free(pipex);
-        exit(EXIT_FAILURE);
+        free(pipex->pid); // Libera a memória alocada para os PIDs se a criação do pipe falhar
+        free(pipex); // Libera a memória alocada para t_pipex se a criação do pipe falhar
+        exit(EXIT_FAILURE); // Sai se a criação do pipe falhar
     }
-    pipex -> path = find_path(envp);
-    return (pipex);
+    
+    // Obtém o caminho PATH do ambiente
+    pipex->path = find_path(envp);
+    
+    return (pipex); // Retorna a estrutura t_pipex inicializada
 }
+
